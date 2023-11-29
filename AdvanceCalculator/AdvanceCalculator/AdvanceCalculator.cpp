29570de396fabd33c0e7f55ac7ef18895f112e43
Isaac Shields <133470230+I-shields@users.noreply.github.com>
2023-11-29 01:19:46 -0500

    Added functions, finished on, and fixed an existing function
    Added a no remainder divide function, also fixed the string function divide to deal with zeros

--- AdvanceCalculator/AdvanceCalculator/AdvanceCalculator.cpp
+++ AdvanceCalculator/AdvanceCalculator/AdvanceCalculator.cpp
@@ -12,6 +12,9 @@ using namespace std;
 int getNum1();
 int getNum2();
 int multi(int x, int y);
+int powers(int y, int x);
+int divideInt(int y, int x);
+double divideDouble(int y, int x);
 string divide(int y, int x);
 
 int main()
@@ -40,7 +43,6 @@ int getNum2()
 int multi(int y, int x)
 {
 	int Solution = 0;
-	cout << "First num: " << x << " Second num: " << y << endl;
 	for (int i = 0; i < y; i++)
 	{
 		Solution = Solution + x;
@@ -51,8 +53,6 @@ int multi(int y, int x)
 
 string divide(int y, int x)
 {
-
-	cout << "First num: " << x << " Second num: " << y << endl;
 	int Temp = x;
 	int i = 0;
 	int Quotient = 0;
@@ -60,41 +60,48 @@ string divide(int y, int x)
 	string Answer = "The answer is ";
 	bool Incomplete = true;
 	bool Invalid = false;
-	if (x != 0)
+	bool ValidDivisor = false;
+
+	while (ValidDivisor == false)
 	{
-		if (Temp == y)
+		if (y == 0)
+		{
+			cout << "Cannot divide by zero, enter a different number: ";
+			cin >> y;
+		}
+		else
+			ValidDivisor = true;
+	}
+
+	if (Temp == y)
+	{
+		Quotient = 1;
+		Incomplete = false;
+	}
+
+	while (Incomplete == true)
+	{
+		if (Temp >= y && Temp - y >= 0)
+		{
+			Temp = Temp - y;
+			i++;
+		}
+		else if (Temp == y || Temp == 0)
 		{
 			Quotient = 1;
 			Incomplete = false;
 		}
-
-		while (Incomplete == true)
+		else if (Temp == 1)
 		{
-			if (Temp >= y && Temp - y >= 0)
-			{
-				Temp = Temp - y;
-				i++;
-			}
-			else if (Temp == y || Temp == 0)
-			{
-				Quotient = 1;
-				Incomplete = false;
-			}
-			else if (Temp == 1)
-			{
-				Remainder = 1;
-				Incomplete = false;
-			}
-			else if (Temp > 1 && Temp < y)
-			{
-				Remainder = Temp;
-				Incomplete = false;
-			}
+			Remainder = 1;
+			Incomplete = false;
 		}
-
+		else if (Temp > 1 && Temp < y)
+		{
+			Remainder = Temp;
+			Incomplete = false;
+		}
 	}
-	else
-		Invalid = true;
 		
 
 	if (Remainder > 0 && Invalid == false)
@@ -103,15 +110,74 @@ string divide(int y, int x)
 	}
 	else
 		Answer = Answer + to_string(i);
+	
+	return Answer;
 
-	if (Invalid == true)
+
+}
+
+int powers(int y, int x)
+{
+	int Solution = x;
+
+	for (int i = 1; i < y; i++)
 	{
-		Answer = "Can't divide by zero.";
+		Solution = multi(Solution, x);
+	}
+	return Solution;
+}
+
+int divideInt(int y, int x)
+{
+	bool Valid = false;
+	bool Incomplete = true;
+	int Quotient;
+	int Temp = x;
+	int i = 0;
+	bool ValidDivisor = false;
+
+	while (ValidDivisor == false)
+	{
+		if (y == 0)
+		{
+			cout << "Cannot divide by zero, enter a different number: ";
+			cin >> y;
+		}
+		else
+			ValidDivisor = true;
 	}
 
-	return Answer;
-	
+	if (x == 0)
+	{
+		Quotient = 0;
+		Incomplete = false;
+	}
 
+	while (Incomplete == true)
+	{
+		if (x == y)
+		{
+			Incomplete = false;
+			Quotient = 1;
+		}
+		else if (Temp >= y && Temp - y >= 0)
+		{
+			Temp = Temp - y;
+			i++;
+		}
+		else
+		{
+			Quotient = i;
+			Incomplete = false;
+		}
 
+	}
 
+	return Quotient;
+		
+}
+
+double divideDouble(int y, int x)
+{
+	return 0;
 }
