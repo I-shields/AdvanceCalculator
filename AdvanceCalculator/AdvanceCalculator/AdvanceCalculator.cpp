461e7925c161ab4d6aab2b1c80a047d786955811
Isaac Shields <133470230+I-shields@users.noreply.github.com>
2023-11-28 21:19:47 -0500

    Initial upload

---
+++ AdvanceCalculator/AdvanceCalculator/AdvanceCalculator.cpp
@@ -0,0 +1,28 @@
+//============================================================
+// Author: Isaac Shields
+// Date  : 11/26/2023
+// Desc  : A calculator that can do more advance math
+//============================================================
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int multi(int, int);
+int getNums();
+
+int main()
+{
+
+}
+
+
+int multi(int x, int y)
+{
+
+}
+
+int getNums()
+{
+
+}
